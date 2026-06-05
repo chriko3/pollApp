b69daf50dff40b9d79e1bbef44bc6bb4a34e343d
chriko3 <christoph.kohout03@gmail.com>
2026-06-05 14:33:10 +0200

    feat(header): add header component with styling
    - Created header-component.scss for styling the header.
    - Implemented HeaderComponent with logoColor input property.

--- src/app/shared/components/home-component/home-component.ts
+++ src/app/shared/components/home-component/home-component.ts
@@ -1,31 +1,19 @@
 import { Component } from '@angular/core';
 import { PrimaryButtonComponent } from '../primary-button-component/primary-button-component';
-import { SecondaryButtonComponent } from '../secondary-button-component/secondary-button-component';
-import { FilterButtonComponent } from "../filter-button-component/filter-button-component";
-import { SurveyViewComponent } from '../survey-view-component/survey-view-component';
-import { SurveryStatusComponent } from '../survery-status-component/survery-status-component';
-import { DeleteButtonComponent } from '../delete-button-component/delete-button-component';
-import { CheckboxComponent } from '../checkbox-component/checkbox-component';
-import { AnswearComponent } from '../answear-component/answear-component';
-import { TertiaryButtonComponent } from '../tertiary-button-component/tertiary-button-component';
+import { HeaderComponent } from '../header-component/header-component';
 import { HighlightCardComponent } from '../highlight-card-component/highlight-card-component';
-import { InputFieldComponent } from '../input-field-component/input-field-component';
+import { FilterButtonComponent } from '../filter-button-component/filter-button-component';
 import { DropDownComponent } from '../drop-down-component/drop-down-component';
 
 @Component({
   selector: 'app-home-component',
-  imports: [PrimaryButtonComponent, SecondaryButtonComponent, FilterButtonComponent,
-    SurveryStatusComponent,
-    DeleteButtonComponent,
-    CheckboxComponent,
-    AnswearComponent,
-    TertiaryButtonComponent,
+  imports: [HeaderComponent,
+    PrimaryButtonComponent,
     HighlightCardComponent,
-    InputFieldComponent,
-    DropDownComponent
-  ],
+    FilterButtonComponent,
+    DropDownComponent],
   templateUrl: './home-component.html',
   styleUrl: './home-component.scss',
 })
-export class HomeComponent { 
+export class HomeComponent {
 }
