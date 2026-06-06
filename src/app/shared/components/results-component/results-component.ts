935c98756117b2c25d442b446aeb28898e090316
chriko3 <christoph.kohout03@gmail.com>
2026-06-06 11:45:58 +0200

    feat(results): enhance results component to display answer statistics and adjust styles

--- src/app/shared/components/results-component/results-component.ts
+++ src/app/shared/components/results-component/results-component.ts
@@ -7,7 +7,16 @@ import { Component, Input } from '@angular/core';
   styleUrl: './results-component.scss',
 })
 export class ResultsComponent {
-  @Input() question?:string;
+  @Input() question?: string;
 
-  
+  @Input() displayAnswers: 2 | 3 | 4 = 2;
+
+  statisticBars: { answer: string; progress: number; }[] = [
+    { answer: 'A', progress: 0 },
+    { answer: 'B', progress: 0 },
+    { answer: 'C', progress: 0 },
+    { answer: 'D', progress: 0 }
+  ];
+
+
 }
